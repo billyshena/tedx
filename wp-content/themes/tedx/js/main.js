90984bc8faeaa2549a5e9d7614a80209d9e05d94
Billy <bshen@ibsciss.com>
2015-04-26 15:40:45 +0200

    [UPDATE] Tedx Landing page

--- wp-content/themes/tedx/js/main.js
+++ wp-content/themes/tedx/js/main.js
@@ -1,13 +1,82 @@
 /* jshint devel:true */
-"use strict"; 
+(function () {
+	"use strict"; 
 
-$(".cbox-html").colorbox({
-	inline:true, 
-	maxHeight:"90%",
-	width:"95%",
-	maxWidth:"700px",
-	scrolling:true,
-	onComplete:function(){
-	
+	var environment = {
+		width: $(window).width(),
+		height: $(window).height()
 	}
-});
+
+	$(window).resize(function(){
+		environment.width = $(window).width();
+		environment.height = $(window).height();
+	});
+
+
+	/********* POPUP **********/
+	
+	var popup = {
+		is_opened: false,
+		transition_duration: 300,
+		open: function(){
+			console.log('open');
+			popup.is_opened = true;
+			$('.popup-container').css({display:"block"}).stop().animate({opacity:1}, popup.transition_duration, function(){
+				$('.popup-container .wrapper>div').stop().animate({"margin-top":(environment.height * 0.2)}, popup.transition_duration);
+			});
+		},
+		close: function(reopen, callback){
+			console.log('close');
+			popup.is_opened = false;
+			$('.popup-container .wrapper>div').stop().animate({"margin-top":(environment.height + 100)}, popup.transition_duration, function(){
+				if(!reopen){
+					$('.popup-container').stop().animate({opacity:0}, popup.transition_duration, function(){
+						$('.popup-container').css({display:"none"});
+						if(callback){
+							callback();
+						}
+					});
+				}else{
+					if(callback){
+						callback();
+					}
+				}
+			});
+		},
+		update: function(href, callback){
+			console.log('update');
+			var temp_html = $(href).html();
+			$('.popup-container .wrapper>div').html(temp_html).removeClass().addClass(href.replace('#',''));
+			cboxAction();
+			callback();
+		}
+	}
+
+	cboxAction();
+
+	function cboxAction(){
+		$(".cbox-html").unbind("click").bind("click", function(e){
+			e.preventDefault();
+			var href = $(this).attr('href');
+			if(!popup.is_opened){
+				popup.update(href, function(){
+					popup.open();
+				});
+			}else{
+				popup.close(true, function(){
+					popup.update(href, function(){
+						popup.open();
+					});
+				});
+			}
+		});
+	}
+
+	$('.popup-container').click(function(e){
+		if(e.target.outerHTML.indexOf('popup-container')>0){
+			popup.close(false);
+		}
+	});
+
+
+})();
--- wp-content/themes/tedx/landing.php
+++ wp-content/themes/tedx/landing.php
@@ -22,8 +22,8 @@ $content = apply_filters('the_content', $form->post_content);
   </head>
   <body class="landing">
     <header class="header">
-      <a href="#" alt="">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="">
+      <a href="#" class="logo" alt "back to home">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/content/logo-black.svg" alt="">
       </a>
       <ul>
         <li><a class="icon-instagram" href="#"></a></li>
@@ -33,31 +33,50 @@ $content = apply_filters('the_content', $form->post_content);
     </header>
     <div class="superglobale">
       <div class="content">
-        <h1><?php echo $post->post_title; ?></h1>
-        <h2><?php echo $post->post_content; ?></h2>
-        <div class="basic-cta">
-          <a href="#popup-speakers" class="icon-mic cbox-html"><p><span>Devenez</span> speakers</p></a>
-          <a href="#popup-partners" class="icon-light-up cbox-html"><p><span>Devenez</span> partenaire</p></a>
-        </div>
+        <h1 class="main-title">Éduquer, c’est élever</h1>
+        <ul class="items clearfix">
+          <li>
+            <h2 class="item-title">Contact</h2>
+            <p class="item-desc"><strong class="bold">TEDxChampsElyseesED</strong><br>est une conférence TEDx qui sera entièrement dédiée à des idées,<br>des réflexions ou des actions<br>concernant l'éducation.</p>
+            <p class="cta-label">Contactez-nous !</p>
+            <a href="mailTo:changemail@gmail.com" class="icon-mail"></a>
+          </li>
+          <li>
+            <h2 class="item-title">Partenaire</h2>
+            <p class="item-desc">Contribuer à l'émergence d’idées nouvelles,<br>à la mise en avant d’actions locales et à favoriser les réflexions<br>autour de l’éducation sont des axes d’engagements qui vous<br>parlent :</p>
+            <p class="cta-label">Rejoignez-nous !</p>
+            <a href="#popup-partners" class="icon-light-up cbox-html"></a>
+          </li>
+          <li>
+            <h2 class="item-title">Intervenant</h2>
+            <p class="item-desc">Vous êtes acteurs ou penseurs de l'éducation, vous êtes parents, vous êtes une entreprise, vous êtes un apprenant et vous voulez partager une idée, une action ou une réflexion concernant l’éducation :</p>
+            <p class="cta-label">Participez !</p>
+            <a href="#popup-speakers" class="icon-mic cbox-html"></a>
+          </li>
+        </ul>
       </div>
     </div>
     <footer>
-      <p>© 2015 TEDx Education</p>
+      <p>© TEDxChampsElyseesED 2015 | This independent TEDx event is operated under license from TED.</p>
     </footer>
-
-
-    <!-- Modal here -->
-    <div class="popup-wrapper" id="popup-speakers">
-        <div class="basic-cta"> <a href="#popup-speakers" class="icon-mic cbox-html active cboxElement"><p><span>Devenez</span> speakers</p></a> <a href="#popup-partners" class="icon-light-up cbox-html cboxElement"><p><span>Devenez</span> partenaire</p></a> </div>
-        <p class="pop-text">Vous avez une idée ou un projet à partager ?<br>Alors candidatez pour peut-être venir en parler sur la scène de TEDx Education !<br>Vous avez jusqu’au 30 juin 2015 pour soumettre votre candidature au comité de programmation. </p>
-        <?php echo $content; ?>
+    <div class="popup-container">
+      <div class="wrapper">
+        <div></div>
+      </div>
     </div>
-    <!-- /Modal here -->
-
+    <div class="popup-wrapper" id="popup-speakers">
+      <div class="basic-cta">
+        <a href="#popup-partners" class="icon-light-up cbox-html"><p><span>Devenez</span> partenaire</p></a>
+        <a href="#popup-speakers" class="icon-mic cbox-html active"><p><span>Devenez</span> speakers</p></a>
+      </div>
+      <p class="pop-text">Vous avez une idée ou un projet à partager ?<br>Alors candidatez pour peut-être venir en parler sur la scène de TEDx Education !<br>Vous avez jusqu’au 30 juin 2015 pour soumettre votre candidature au comité de programmation.
+      </p>
+      <?php echo $content; ?>
+    </div>
     <div class="popup-wrapper" id="popup-partners">
       <div class="basic-cta">
+        <a href="#popup-partners" class="icon-light-up cbox-html active"><p><span>Devenez</span> partenaire</p></a>
         <a href="#popup-speakers" class="icon-mic cbox-html"><p><span>Devenez</span> speakers</p></a>
-        <a href="#popup-partners" class="icon-light-up cb-html active"><p><span>Devenez</span> partenaire</p></a>
       </div>
       <p class="pop-text">Devenir partenaire de TEDxParis c’est engager un dialogue avec une audience exclusive, passionnée et ouverte d’esprit.</p>
       <p class="pop-text">TEDxParis est un évènement qui rencontre toujours plus de succès, un des plus prestigieux TEDx mondial. L’édition 2014 aura lieu au Théâtre du Châtelet, devant plus de 1600 personnes. Un lieu mythique pour une visibilité privilégiée, où nous ne manquerons de valoriser votre marque et vos valeurs.</p>
