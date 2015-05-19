a5f39449df7f86ffab51c7d8a29081a3badd1843
Billy Shen <bshen@ibsciss.com>
2015-05-19 16:04:03 +0200

    [ADD] Facebook API implementation

--- wp-content/themes/tedx/index.php
+++ wp-content/themes/tedx/index.php
@@ -22,8 +22,6 @@ $twitter = new TwitterAPIExchange($settings);
     ->performRequest();*/
 
 
-
-
 /** INSTAGRAM API CALL */
 
 function fetchData($url){
@@ -38,6 +36,69 @@ function fetchData($url){
 
 $result = fetchData("https://api.instagram.com/v1/users/self/feed?access_token=2122502468.1fb234f.af1670cd239445f9bbd8757360edd837");
 $result = json_decode($result);
+
+
+
+/* FACEBOOK API CALL */
+require __DIR__ . '/facebook-php-sdk-v4/autoload.php';
+
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication('847250295363160','2e2cab6280675a800189840884298cc5');
+
+// Use one of the helper classes to get a FacebookSession object.
+//   FacebookRedirectLoginHelper
+//   FacebookCanvasLoginHelper
+//   FacebookJavaScriptLoginHelper
+// or create a FacebookSession with a valid access token:
+$session = new FacebookSession('847250295363160|iBtUYnwSYafZrI7j6CmpdLGCUUg');
+
+// Get the GraphUser object for the current user:
+
+try {
+    $me = (new FacebookRequest(
+        $session, 'GET', '/1550969358467114/feed'
+    ))->execute()->getGraphObject(GraphUser::className());
+    $data = $me->getProperty('data');
+
+    $results_array = $data->asArray();
+
+
+    foreach($results_array as $fb){
+        if($fb->object_id){
+            $request = new FacebookRequest(
+                $session,
+                'GET',
+                '/'.$fb->object_id
+            );
+            $response = $request->execute();
+            $graphObject = $response->getGraphObject();
+
+        }
+    }
+
+
+} catch (FacebookRequestException $e) {
+    print_r($e);
+    // The Graph API returned an error
+} catch (\Exception $e) {
+    print_r($e);
+    // Some other error occurred
+}
+
+
+
+
+
+
+
+
+
+
 ?>
 
 <h1>Hello world</h1>
